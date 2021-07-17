a93da07d932ad41513c49286fdd3b0af6dd288e9
robg916 <86701371+robg916@users.noreply.github.com>
2021-07-16 22:35:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function compute()
+{
+  p = document.getElementById("p").value;
+  n = document.getElementById("n").value;
+  r = document.getElementById("r").value;
+  result = document.getElementById("result");
+  
+  result.innerHTML = "The interest is " + (p*n*r/100);
+}
